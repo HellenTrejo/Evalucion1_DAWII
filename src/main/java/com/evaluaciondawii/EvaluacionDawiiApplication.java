406064ab3319a350e5a95050a1eeacca17e2a52c
Hellen <hellentrejo@gmail.com>
2020-05-27 01:02:39 -0500

    Primer commit

---
+++ src/main/java/com/evaluaciondawii/EvaluacionDawiiApplication.java
@@ -0,0 +1,13 @@
+package com.evaluaciondawii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionDawiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionDawiiApplication.class, args);
+	}
+
+}
