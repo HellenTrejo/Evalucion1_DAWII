37329ee4c84ffb3c745623cd56e4dae6555f7574
Hellen <hellentrejo@gmail.com>
2020-05-27 14:00:07 -0500

    Agregando interfaces Categoria y Proveedor al repositorio

---
+++ src/main/java/com/evaluaciondawii/repositorio/CategoriaRepositorio.java
@@ -0,0 +1,12 @@
+package com.evaluaciondawii.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.evaluaciondawii.entidad.Categoria;
+
+
+public interface CategoriaRepositorio extends JpaRepository<Categoria, Integer>  {
+
+	
+	
+}
---
+++ src/main/java/com/evaluaciondawii/repositorio/ProveedorRepositorio.java
@@ -0,0 +1,12 @@
+package com.evaluaciondawii.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.evaluaciondawii.entidad.Proveedor;
+
+
+public interface ProveedorRepositorio extends JpaRepository<Proveedor, Integer>  {
+
+	
+	
+}
